9c7b96db139de6c7ae2f5121153db53dc2b90587
RossCabrera <cabrera.ross@outlook.es>
2026-05-01 13:21:26 -0600

    fix(auth): correct relative import for models in serializers

--- backend/apps/authentication/serializers.py
+++ backend/apps/authentication/serializers.py
@@ -1,4 +1,4 @@
-from models import User, UserManager
+from .models import User, UserManager
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
