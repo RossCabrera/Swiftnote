d3fb94d8e9a13c9ba5d71b84da74623a2ee767df
RossCabrera <cabrera.ross@outlook.es>
2026-05-08 04:38:00 -0600

    feat(app): integrate authentication into app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { SwiftnoteApp } from './SwiftnoteApp'
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SwiftnoteApp />
-  </StrictMode>,
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID as string}>
+      <SwiftnoteApp />
+    </GoogleOAuthProvider>
+  </StrictMode>
 )
